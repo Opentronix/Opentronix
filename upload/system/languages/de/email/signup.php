9a3789c7bc6571d9424a4afe32a79c2f76a34f94
Elias Diem <pub.scm@webconect.ch>
2014-07-06 19:38:27 +0200

    Typos

--- upload/system/languages/de/email/signup.php
+++ upload/system/languages/de/email/signup.php
@@ -1,10 +1,10 @@
 <?php
 
 $lang = array (
-  'signup_email_subject' => 'Bestätigung deiner Anmeldung auf #SITE_TITLE#',
+  'signup_email_subject' => 'Bestätigung Deiner Anmeldung auf #SITE_TITLE#',
   'signup_email_hello' => 'Hallo,',
-  'signup_email_message' => 'Zur Bestätigung deiner Anmeldung auf #SITE_TITLE#, benutze bitte den folgenden Link:',
-  'signup_email_warning' => 'Falls du keine Registrierung angefordert hast, ignorieren bitte diese Nachricht.',
+  'signup_email_message' => 'Zur Bestätigung Deiner Anmeldung auf #SITE_TITLE#, benutze bitte den folgenden Link:',
+  'signup_email_warning' => 'Falls Du keine Registrierung angefordert hast, ignoriere bitte diese Nachricht.',
   'signup_email_signature' => 'Mit herzlichen Grüßen,
 #SITE_TITLE#',
 )
